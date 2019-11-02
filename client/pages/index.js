2be6593007d5feaf8c64fd0663b561ecca7571b3
Andrée Höög <hoog16@gmail.com>
2019-11-02 19:22:52 +0100

    Change header

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,14 +5,14 @@ import Nav from '../components/nav';
 const Home = () => (
     <div>
         <Head>
-            <title>Home for Lonch2</title>
+            <title>Home</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <Nav />
 
         <div className="hero">
-            <h1 className="title">Welcome to Next.js!</h1>
+            <h1 className="title">Welcome to Lonch 2.0</h1>
             <p className="description">
                 To get started, edit <code>pages/index.js</code> and save to
                 reload.
