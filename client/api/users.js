31ca9ff60994837b095a26f7df099897a6eb92ea
Andrée Höög <hoog16@gmail.com>
2019-11-03 22:17:03 +0100

    Return dummy user data

--- client/api/users.js
+++ client/api/users.js
@@ -32,14 +32,16 @@ module.exports = async (req, res) => {
     // Get a database connection, cached or otherwise,
     // using the connection string environment variable as the argument
     console.log('MONGODB_URI', process.env.MONGODB_URI);
-    const db = await connectToDatabase(process.env.MONGODB_URI);
+    // const db = await connectToDatabase(process.env.MONGODB_URI);
 
-    // Select the "users" collection from the database
-    const collection = await db.collection('users');
+    // // Select the "users" collection from the database
+    // const collection = await db.collection('users');
 
-    // Select the users collection from the database
-    const users = await collection.find({}).toArray();
-
+    // // Select the users collection from the database
+    // const users = await collection.find({}).toArray();
+    const users = {
+        name: 'apa'
+    };
     // Respond with a JSON string of all users in the collection
     res.status(200).json({ users });
 };
